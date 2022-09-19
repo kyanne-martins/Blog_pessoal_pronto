091d7aa50bfd220efd8f3eea7061b2d2adf55b9c
kyanne-martins <kyanne801@gmail.com>
2022-09-19 19:52:19 -0300

    Deploy do blog pessoal

---
+++ src/app/tema/tema.component.ts
@@ -0,0 +1,47 @@
+import { TemaService } from './../service/tema.service';
+import { Tema } from './../model/Tema';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+ }
+
+}
